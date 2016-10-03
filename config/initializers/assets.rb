7a2b34fc38bebf161af5de645a75732628c0762c
carlito <pochy9024@gmail.com>
2016-10-02 20:47:07 -0400

    add js to assets initializer

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,23 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+  Rails.application.config.assets.precompile += %w( vendor/jquery-1.11.1.js )
+  Rails.application.config.assets.precompile += %w( vendor/jquery-migrate-1.2.1.min.js )
+  Rails.application.config.assets.precompile += %w( twitterFetcher_min.js )
+  Rails.application.config.assets.precompile += %w( vendor/bootstrap.js )
+  Rails.application.config.assets.precompile += %w( wow.min.js )
+  Rails.application.config.assets.precompile += %w( imagesloaded.pkgd.min.js )
+  Rails.application.config.assets.precompile += %w( jquery.easing.min.js )
+  Rails.application.config.assets.precompile += %w( appear.js )
+  Rails.application.config.assets.precompile += %w( vendor/modernizr-2.8.3.min.js )
+  Rails.application.config.assets.precompile += %w( queryloader2.min.js )
+  Rails.application.config.assets.precompile += %w( jquery.circliful.min.js )
+  Rails.application.config.assets.precompile += %w( owl.carousel.min.js )
+  Rails.application.config.assets.precompile += %w( nivo-lightbox.min.js )
+  Rails.application.config.assets.precompile += %w( isotope.pkgd.min.js )
+  Rails.application.config.assets.precompile += %w( notie.js )
+  Rails.application.config.assets.precompile += %w( main.js )
+
+
+
+
